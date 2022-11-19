60921dd64376314c28d51fd2ebeca21939f5e610
Christian <christianstewart5111@gmail.com>
2022-11-19 16:40:48 +0800

    Added PDF downloader

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,27 +84,42 @@ themeButton.addEventListener('click', () => {
 })
 
 /*==================== REDUCE THE SIZE AND PRINT ON AN A4 SHEET ====================*/ 
-
+function scaleCV() {
+    document.body.classList.add('scale-cv')
+}
 
 /*==================== REMOVE THE SIZE WHEN THE CV IS DOWNLOADED ====================*/ 
-
+function removeScale() {
+    document.body.classList.remove('scale-cv')
+}
 
 /*==================== GENERATE PDF ====================*/ 
 // PDF generated area
-
+let areaCv = document.getElementById('area-cv')
+let resumeButton = document.getElementById('resume-button')
 
 // Html2pdf options
-
+let opt = {
+    margin:       1,
+    filename:     'StewartResume.pdf',
+    image:        { type: 'png', quality: 0.98 },
+    html2canvas:  { scale: 4 },
+    jsPDF:        { format: 'a4', orientation: 'portrait' }
+};
 
 // Function to call areaCv and Html2Pdf options 
-
+function generateResume() {
+    html2pdf(areaCv, opt)
+}
 
 // When the button is clicked, it executes the three functions
-
+resumeButton.addEventListener('click', () => {
     // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
-
+    scaleCV()
 
     // 2. The PDF is generated
-
+    generateResume()
 
     // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
+    setTimeout(removeScale, 3000)
+})
